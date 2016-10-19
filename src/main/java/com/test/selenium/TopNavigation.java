c0c81305a95c18e4fd1fe350659e6a4142f14ab7
bogdanovan <bogdanovan88@gmail.com>
2016-10-19 23:10:19 +0300

    Update TopNavigation.java

--- src/main/java/com/test/selenium/TopNavigation.java
+++ src/main/java/com/test/selenium/TopNavigation.java
@@ -11,17 +11,5 @@ import java.util.concurrent.TimeUnit;
  * Created by baboonik on 10/18/16.
  */
 public class TopNavigation {
-//    public static void main(String[] args) {
-//        WebDriver driver = new SafariDriver();
-//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//        driver.get("http://eu.wiley.com/WileyCDA/");
-//
-//        WebElement bodyElement = driver.findElement(By.id("homepage-links"));
-//        WebElement ulElement = bodyElement.findElement(By.xpath("//*[@id=\"homepage-links\"]/ul"));
-//        List<WebElement> liElements = ulElement.findElements(By.className("li"));
-//        for(WebElement childLi : liElements){
-//            List fieldSetEle = childLi.findElements(By.tagName("fieldset"));
-//            System.out.println(childLi.getTagName()+":"+fieldSetEle.size());
-//        }
-//    }
+
 }
